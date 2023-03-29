abb3208b1f55a7b934107e0d02556989b0c919d0
Ting hon Linus cheung <linuscheung@Tings-MBP-2.home>
2023-03-28 23:41:07 -0700

    added new codes in JS

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,30 @@
-var topic = "HTML";
-
-if (topic === 'HTML') {
-    console.log("Let's study HTML!");
-
-} else if (topic === 'CSS') {
-    console.log("Let's study CSS!");
-
-} else if (topic === 'Git') {
-    console.log("let's stuy Git!");
-
-} else if (topic === "JavaScript") {
-    console.log("Let's stuy JavaScript")
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript']
+for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
 
 }
-else { console.log('Please try again!') }
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
+
 
 var shapes = ["triangle", "square", "pentagon", "circle"];
 console.log(shapes[0]);
 console.log(shapes[1]);
 console.log(shapes[2]);
 console.log(shapes[3]);
+
+for (let x = 0; x < shapes.length; x++) {
+    console.log(shapes[x])
+}
